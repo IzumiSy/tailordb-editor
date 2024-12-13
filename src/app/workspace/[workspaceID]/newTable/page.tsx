2df274ce385e7ed465a960a2b5ed687307ad2260
IzumiSy <beetle-noise@gmx.com>
2024-12-13 19:56:41 +0900

    Add new table page

--- src/app/workspace/[workspaceID]/newTable/page.tsx
+++ src/app/workspace/[workspaceID]/newTable/page.tsx
@@ -1,5 +1,34 @@
-const Page = () => {
-  return <div>New table</div>;
+import { Stack, Flex, HStack, Button, Input } from "@chakra-ui/react";
+import Link from "next/link";
+
+type PageProps = {
+  params: Promise<{
+    workspaceID: string;
+  }>;
+};
+
+const Page = async (props: PageProps) => {
+  const params = await props.params;
+
+  return (
+    <Stack width={"100%"} gap={0}>
+      <Flex p={2} justifyContent={"space-between"} width={"100%"} gap={2}>
+        <Input placeholder="Table name" size="xs" />
+        <HStack>
+          <Button size="xs">Save changes</Button>
+          <Button
+            as={Link}
+            // @ts-ignore
+            href={`/workspace/${params.workspaceID}`}
+            size="xs"
+            variant="ghost"
+          >
+            Close
+          </Button>
+        </HStack>
+      </Flex>
+    </Stack>
+  );
 };
 
 export default Page;
